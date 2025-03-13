2f0542087ee66616ab30c9b0f3242ebacce9fc4b
juandavidcertuche93 <juandavidcertuchequijano2@gmail.com>
2025-03-13 15:56:11 -0500

    Implement skeleton component for pokemon list loading, and update pokemon page.

--- src/app/pages/pokemons/pokemons-page.component.ts
+++ src/app/pages/pokemons/pokemons-page.component.ts
@@ -1,5 +1,7 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ApplicationRef, ChangeDetectionStrategy, Component, inject, OnInit, signal } from '@angular/core';
 import { PokemonListComponent } from "../../pokemons/components/pokemon-list/pokemon-list.component";
+import { PokemonListSkeletonComponent } from "./ui/pokemon-list-skeleton/pokemon-list-skeleton.component";
+import { log } from 'console';
 
 @Component({
   selector: 'pokemons-page',
@@ -7,6 +9,20 @@ import { PokemonListComponent } from "../../pokemons/components/pokemon-list/pok
   templateUrl: './pokemons-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export default class PokemonsPageComponent {
+export default class PokemonsPageComponent implements OnInit {
 
+  // public isLoading = signal(true)
+
+  // private appRef = inject(ApplicationRef)
+
+  // private $appState = this.appRef.isStable
+  // .subscribe( isStable => {
+  //   console.log({isStable});
+  // })
+
+  ngOnInit(): void {
+    // setTimeout(() => {
+    //   this.isLoading.set(false)
+    // }, 5000)
+  }
 }
